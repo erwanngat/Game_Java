935803d57a2aa07b515c78a1f90f85671c0b6b90
erwanngat <ewnran69@yahoo.fr>
2024-04-14 22:57:28 +0200

    up menu view

--- src/main/java/com/example/game_java/controller/game/MenuController.java
+++ src/main/java/com/example/game_java/controller/game/MenuController.java
@@ -1,14 +1,26 @@
 package com.example.game_java.controller.game;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 
 public class MenuController {
 
+    public Button slot1;
+    public Button slot2;
+    public Button slot3;
+    public Label labelSlot1;
+    public Label labelSlot2;
+    public Label labelSlot3;
     private Stage stage;
 
     public void setStage(Stage stage) {
         this.stage = stage;
     }
-    
+
+    public void onNewCharacterButton(ActionEvent actionEvent) {
+        System.out.println("new char");
+    }
 }
